a6b780f4a26c5e4927f7464c65a9fb253d2066cc
Sardo Ip <sardo@pixelactionstudio.com>
2022-10-25 09:21:41 +0800

    Merge import part

--- import.php
+++ import.php
@@ -11,43 +11,45 @@ $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load('charas.xlsx');
 $currSheet = $spreadsheet->getSheet(0);
 
 $rows = $currSheet->getHighestRow();
-for($i = 2; $i <= $rows; $i++){
-  $chara_name = $currSheet->getCell('A' . $i)->getFormattedValue();
-  $text_count = $currSheet->getCell('B' . $i)->getFormattedValue();
-  $exp = $currSheet->getCell('C' . $i)->getFormattedValue();
+for($i = 1; $i <= $rows; $i++){
+  $chara_name = $currSheet->getCell('A' . $i)->getValue();
+  $text_count = $currSheet->getCell('B' . $i)->getValue();
+  $exp = $currSheet->getCell('C' . $i)->getValue();
+  $items = $currSheet->getCell('E' . $i)->getValue();
+  $qq = $currSheet->getCell('F' . $i)->getValue();
 
-  $related_chara_json = searchChara($search_api_url, $chara_name);
-  $related_chara = json_decode($related_chara_json);
-
-  if($related_chara){
-    $chara_id = $related_chara->ID;
-    $update_values = [
-      'id' => $chara_id,
-      'textCount' => $text_count,
-      'exp' => $exp,
-    ];
-    $update_result = updateChara($import_api_url, $update_values);
+  if($chara_name && $qq){
+    $charas = json_decode(searchChara($search_api_url, $qq), TRUE);
+    foreach($charas as $chara){
+      $update_content = json_encode([
+        'id' => $chara['ID'],
+        'textCount' => $text_count,
+        'exp' => $exp,
+        'items' => $items,
+      ]);
+      $result = json_decode(updateChara($import_api_url, $update_content, TRUE));
+      var_dump($result);
+    }
   }
 }
 
-function searchChara($search_api_url, $chara_name){
+function searchChara($search_api_url, $qq){
+  $post_content = json_encode(['qq' => $qq]);
   $curl = curl_init();
   curl_setopt($curl, CURLOPT_URL, $search_api_url);
   curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($curl, CURLOPT_POST, true);
-  curl_setopt($curl, CURLOPT_POSTFIELDS, [
-    'title' => $chara_name,
-  ]);
+  curl_setopt($curl, CURLOPT_POSTFIELDS, $post_content);
+  curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
   $result = curl_exec($curl);
   curl_close($curl);
   return $result;
 }
-function updateChara($import_api_url, $update_values){
+function updateChara($import_api_url, $update_content){
   $curl = curl_init();
   curl_setopt($curl, CURLOPT_URL, $import_api_url);
   curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($curl, CURLOPT_POST, true);
-  curl_setopt($curl, CURLOPT_POSTFIELDS, $update_values);
+  curl_setopt($curl, CURLOPT_POSTFIELDS, $update_content);
+  curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
   $result = curl_exec($curl);
   curl_close($curl);
   return $result;
